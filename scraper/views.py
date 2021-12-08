a847d1367dff154bb22986d0fed6c31dcc20dbe0
RubberTrampIT <rubbertrampit@gmail.com>
2021-12-08 11:14:44 -0700

    commit

--- scraper/views.py
+++ scraper/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
-import requests
-response = requests.get("https://oxylabs.io/”)
-print(response.text)
+from django.http import HttpResponse
+def index(request):
+    #return HttpResponse("Hello, mars")
+    import requests
+    response = requests.get("https://oxylabs.io/”)
+    return HttpResponse(response.text)
